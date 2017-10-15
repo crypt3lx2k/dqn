21184406db193532b6b992c4460e20adcce7b92a
Truls Edvard Stokke <trulses@gmail.com>
2017-10-16 00:17:14 +0200

    Moved to more compressed data storage.

--- replay.py
+++ replay.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 from six.moves import xrange
 
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 
 class Experience (object):
     def __init__ (self, capacity, observation_shape, action_shape):
@@ -14,14 +15,27 @@ class Experience (object):
         self.shape = (capacity,)
         self.state_shape = self.shape + observation_shape
 
+        frame_channels = self.state_shape[-1]
+        self.storage_shape = self.shape + observation_shape[:-1] + (frame_channels+1,)
+
         # Initialize memory, due to overcommitted memory you can allocate more
         # than the free memory here. Change allocator from np.empty to np.ones
         # if you need guarantees ahead of time.
-        self.observations = np.empty(self.state_shape, dtype=np.uint8)
-        self.successors = np.empty(self.state_shape, dtype=np.uint8)
         self.actions = np.empty(self.shape + action_shape, dtype=np.int8)
         self.rewards = np.empty(self.shape, dtype=np.int8)
         self.terminals = np.empty(self.shape, dtype=np.bool)
+
+        # For stacked frame storage we utilize the fact that observation and successor
+        # may at most differ by 1 frame, so we store a stack of frame_channels+1 and
+        # provide strided view into this storage.
+        self.storage = np.empty(self.storage_shape, dtype=np.uint8)
+
+        self.observations = as_strided (
+            self.storage[...,0], shape=self.state_shape, strides=self.storage.strides
+        )
+        self.successors = as_strided (
+            self.storage[...,1], shape=self.state_shape, strides=self.storage.strides
+        )
 
         # Index points to current open slot and will wrap around
         self.index = self.capacity
@@ -37,7 +51,7 @@ class Experience (object):
         self.observations[self.index] = observation
         self.actions[self.index] = action
         self.rewards[self.index] = reward
-        self.successors[self.index] = successor
+        self.successors[self.index,...,-1] = successor[...,-1]
 
         # Also keep track of terminal states
         self.terminals[self.index] = terminal
